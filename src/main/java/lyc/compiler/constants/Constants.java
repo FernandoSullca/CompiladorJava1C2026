943b763de0907828b863d93f5e8f3668be81c943
FernandoSullca <sullcafernando18@gmail.com>
2026-04-27 11:49:43 -0300

    update constantes para cotas negativas

--- src/main/java/lyc/compiler/constants/Constants.java
+++ src/main/java/lyc/compiler/constants/Constants.java
@@ -3,14 +3,14 @@ package lyc.compiler.constants;
 public final class Constants {
 
     public static final int MAX_LENGTH = 40; //MAXIMO DE CARACTERES DE STRING
-
+    // Int (16 bits): Rango con signo
     public static final int MAX_INT = 32767;
-    
-    public static final int MIN_INT = 0;
 
-    public static final double MAX_FLOAT = 65504.0 ;
-    
-    public static final double MIN_FLOAT = 0;
+    public static final int MIN_INT = -32768;
+    // Float (32 bits): Rango de 32 bits lineal para pruebas
+    public static final double MAX_FLOAT =  2147483648.0;
+
+    public static final double MIN_FLOAT = -2147483648.0;
 
     private Constants(){}
 
